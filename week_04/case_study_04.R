0a8f1cf1f9ee3a0f3dc3d705290e735c9666faf3
Fan Li <43713448+FanLukeLi@users.noreply.github.com>
2021-09-28 10:40:31 -0400

    commit message

--- week_04/case_study_04.R
+++ week_04/case_study_04.R
@@ -1,53 +1,41 @@
-# There are only two airports in "airports" in New York city, 
-# they are West 30th St. Heliport(JRA) and John F Kennedy Intl(JFK). 
-# In "flights", no flights with JRA as origin or destination can be found and
-# there are no flights with JFK as destination. 
-
-# That is why the final solution to this problem is to 
-# filter flights with JFK as origin and arrange the data by distance
-# in descendant order, then left join airports by destination code to
-# lead to the final dataset. Finally, just return the name of the first
-# row of the final dataset, which would be name of the farthest airport
-# of any airports in NYC
-# Four steps taken. 
-
 library(tidyverse)
 library(nycflights13)
 
-flights_JFK_origin <- flights %>% 
-  filter(origin == "JFK") %>% 
-  arrange(desc(distance))
-
-# flights_JFK_dest <- flights %>% 
-#   filter(dest == "JFK") %>% 
-#   arrange(desc(distance))
-
-# flights_JRA <- flights %>% 
-#   filter(origin =="JRA" | dest == "JRA") %>% 
-#   arrange(desc(distance))
-
-View(flights_JFK)
-# View(flights_JRA)
-
-joined_flights_jfk_origin <- flights_JFK_origin %>% 
-  left_join(airports, by = c("dest" = "faa"))
-
-# joined_flights_jfk_dest <- flights_JFK_dest %>% 
-#   left_join(airports, by = c("dest" = "faa"))
-
-joined_flights_jfk_origin
-
-# joined_flights_jfk_dest
-
-joined_flights_jfk_origin$name[1]
+View(flights)
+View(airports)
 
 ################################################################################ 
-# All OF THE ABOVE LEADS TO: 
-Joined_flights_jfk_origin <- flights %>% 
-  filter(origin == "JFK") %>% 
+Joined_flights_origin <- flights %>% 
+  filter(origin == "JFK" | origin == "LGA" | origin == "EWR") %>% 
   arrange(desc(distance)) %>% 
-  left_join(airports, by = c("dest" = "faa"))
+  left_join(airports, by = c("dest" = "faa", "origin" == "faa"))
 
-Joined_flights_jfk_origin$name[1]
+Joined_flights_dest <- flights %>% 
+  filter(dest == "JFK" | dest == "LGA" | dest == "EWR") %>% 
+  arrange(desc(distance)) %>% 
+  left_join(airports, by = c("origin" = "faa"))
+
+# joined_flights <- flights %>%
+#   filter(origin == "JFK" | origin == "LGA" | origin == "EWR" | dest == "JFK" | dest == "LGA" | dest == "EWR") %>%
+#   right_join(airports, by = c("origin" = "faa", "dest" = "faa")) %>%
+#   arrange(desc(distance))
+# This doesn't work, for: 
+# Error Join columns must be unique
+
+farthest_airport_origin <- Joined_flights_origin$name[1]
+distance_1 <- Joined_flights_origin$distance[1]
+
+farthest_airport_dest <- Joined_flights_dest$name[1]
+distance_2 <- Joined_flights_dest$distance[1]
+
+if (distance_1 < distance_2){
+  farthest_airport <- farthest_airport_dest
+} else if (distance_1 > distance_2) {
+  farthest_airport <- farthest_airport_origin
+} else {
+  farthest_airport <- farthest_airport_dest
+}
+
+farthest_airport
 
 ################################################################################
